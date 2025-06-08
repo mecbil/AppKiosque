620a874da8e2f4eff91683c8eef8eba05f2fa8f7
Nabil <mecbil11@gmail.com>
2025-06-08 11:57:43 +0200

    Install Webpack, PostCSS, and Tailwind CSS for CSS processing and styling

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
